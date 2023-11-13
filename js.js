3d145dc4ac47b117545a42fde2de90907877c6f4
yura demyanenko <setka000@gmail.com>
2023-11-13 18:37:21 +0200

    change_<br><br><br>

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-document.write('<b>Вивести на сторінку в один рядок через кому числа від 10 до 20:</b>'+ '/br>'+'/br>');
+document.write('<b>Вивести на сторінку в один рядок через кому числа від 10 до 20:</b>' + '<br><br>');
 
 let num1=10;
 let num2=21;
@@ -11,10 +11,10 @@ for(let i=num1; i < num2; i++) {
         document.write(i + ", &nbsp;" )
     }
 } 
-document.write('<br><br><br>');
+document.write('<br><br>');
 
 
-document.write('<b>Вивести квадрати чисел від 10 до 20:</b>'+ '<br><br>');
+document.write('<b>Вивести квадрати чисел від 10 до 20:</b>' + '<br><br>');
 
 for (i = num1; i < num2; i++) {
     if (i == num2 - 1) {
@@ -23,11 +23,11 @@ for (i = num1; i < num2; i++) {
         document.write(i**2 + ", &nbsp;");
     }
 }
-    document.write('<br><br><br>');
+    document.write('<br><br>');
 
 
 
-document.write('<b>Вивести таблицю множення на 7:</b>'+ '<br><br>');
+document.write('<b>Вивести таблицю множення на 7:</b>' + '<br><br>');
 
 let num3 = 1;
 let num4 = 7;
@@ -38,10 +38,10 @@ for(let i = num3; i < num4; i++) {
     document.write(`${i} * ${num4} = ${sum}`);
     document.write('<br>');
 }
-document.write('<br><br>');
+document.write('<br>');
 
 
-document.write('<b>Знайти суму всіх цілих чисел від 1 до 15:</b>'+ '<br><br>');
+document.write('<b>Знайти суму всіх цілих чисел від 1 до 15:</b>' + '<br><br>');
 
 let num5 = 1;
 let num6 = 15;
@@ -50,10 +50,10 @@ let sum = 0;
 for(let i = num5; i < num6; i++) {
     sum += i;
 }
-document.write(sum + "<br>" + "<br>" + "<br>");
+document.write(sum + "<br><br>");
 
 
-document.write('<b>Знайти добуток усіх цілих чисел від 15 до 35:</b>'+ '<br><br>');
+document.write('<b>Знайти добуток усіх цілих чисел від 15 до 35:</b>' + '<br>');
 
 num5 = 15;
 num6 = 35;
@@ -62,10 +62,10 @@ sum = 1;
 for(let i = num5; i < num6; i++) {
     sum *= i;
 }
-document.write(sum + "<br>" + "<br>" + "<br>");
+document.write(`<p> ${sum} </p>`);
 
 
-document.write('<b>Знайти середнє арифметичне всіх цілих чисел від 1 до 500:</b>'+ '<br><br>');
+document.write('<b>Знайти середнє арифметичне всіх цілих чисел від 1 до 500:</b>' + '<br>');
 
 num5 = 1;
 num6 = 500;
@@ -74,10 +74,10 @@ sum = 0;
 for(let i = num5; i <= num6; i++) {
     sum += i;
 }
-document.write(sum/num6 + "<br>" + "<br>" + "<br>");
+document.write(`<p> ${sum/num6} </p>`);
 
 
-document.write('<b>Вивести суму лише парних чисел в діапазоні від 30 до 80:</b>'+ '<br><br>');
+document.write('<b>Вивести суму лише парних чисел в діапазоні від 30 до 80:</b>' + '<br>');
 
 num5 = 30;
 num6 = 80;
@@ -88,10 +88,10 @@ for(let i = num5; i <= num6; i++) {
         sum += i;
     }
 }    
-document.write(sum + "<br>" + "<br>" + "<br>");
+document.write(`<p> ${sum} </p>`);
 
 
-document.write('<b>Вивести всі числа в діапазоні від 100 до 200 кратні 3:</b>'+ '<br><br>');
+document.write('<b>Вивести всі числа в діапазоні від 100 до 200 кратні 3:</b>' + '<br><br>');
 
 num5 = 100;
 num6 = 200;
@@ -101,10 +101,10 @@ for(let i = num5; i <= num6; i++) {
         document.write(i + "&nbsp;&nbsp;")
     }
 }    
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
-document.write('<b>Дано натуральне число 8. Знайти та вивести на сторінку всі його дільники:</b>'+ '<br><br>');
+document.write('<b>Дано натуральне число 8. Знайти та вивести на сторінку всі його дільники:</b>' + '<br><br>');
 
 num5 = 1;
 num6 = 8;
@@ -114,10 +114,10 @@ for(let i = num5; i <= num6; i++) {
         document.write(i + "&nbsp;&nbsp;")
     }
 }    
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
-document.write('<b>Визначити кількість його парних дільників:</b>'+ '<br><br>');
+document.write('<b>Визначити кількість його парних дільників:</b>' + '<br><br>');
 
 num5 = 1;
 num6 = 8;
@@ -131,10 +131,10 @@ for(let i = num5; i <= num6; i++) {
 }    
 document.write(sum + "&nbsp;&nbsp;")
 
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
-document.write('<b>Знайти суму його парних дільників:</b>'+ '<br><br>');
+document.write('<b>Знайти суму його парних дільників:</b>' + '<br><br>');
 
 num5 = 1;
 num6 = 8;
@@ -147,10 +147,10 @@ for(let i = num5; i <= num6; i++) {
 }    
 document.write(sum + "&nbsp;&nbsp;")
 
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
-document.write('<b>Надрукувати повну таблицю множення від 1 до 10:</b>'+ '<br><br>');
+document.write('<b>Надрукувати повну таблицю множення від 1 до 10:</b>' + '<br><br>');
 
 
 
@@ -170,7 +170,7 @@ for(let i = 1; i <= num5; i++) {
 }
 
 
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
 for (let i = 1; i <= num5; i++) {
@@ -181,10 +181,10 @@ for (let i = 1; i <= num5; i++) {
     }
     document.write("<br>");
 }
-document.write("<br>" + "<br>");
+document.write("<br>");
 
 
-document.write('<b>Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….):</b>'+ '<br><br>');
+document.write('<b>Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….):</b>' + '<br><br>');
 
 num5 = 19.5;
 num6 = 29.5;
@@ -200,10 +200,10 @@ for(let i = num5; i <= num6; i = i+0.5) {
     document.write(result + " " )
     }
 
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
-document.write('<b>Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів:</b>'+ '<br><br>');
+document.write('<b>Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів:</b>' + '<br><br>');
 
 num5 = 27;
 num6 = 10;
@@ -215,10 +215,10 @@ for(let i = num6; i <= num8; i = i+num6) {
     document.write(i + " доларів коштує " + (sum) + "&nbsp;гривень<br>")
     }
 
-document.write("<br><br><br>");
+document.write("<br>");
 
 
-document.write('<b>Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N:</b>'+ '<br><br>');
+document.write('<b>Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N:</b>' + '<br><br>');
 
 num1 = 1;
 num2 = 100;
@@ -230,7 +230,7 @@ for (i = num1; i < num2; i++) {
         document.write(`${i} ,  `);
     }
 }
-    document.write('<br><br><br>');
+    document.write('<br><br>');
 
 
 document.write('<b>Дане ціле число. З"ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе):</b>'+ '<br><br>');   
@@ -244,7 +244,7 @@ if (num1 == 2 || num1 == 3 || num1 == 5 || num1 == 7) {
 } else if ( num1 % 3 == 0 || num1 % 2 == 0 ) {
     document.write(`"${num1}" - не просте число`);
 } else 
-    document.write((num1) + " - просте число <br><br><br>");
+    document.write((num1) + " - просте число <br><br>");
 
 for (let i = num2; i < num3; i++) {
     if ( i == 2 ) {
@@ -261,7 +261,7 @@ for (let i = num2; i < num3; i++) {
     }
 }
 
-document.write("<br><br><br>");
+document.write("<br><br>");
 
 
 document.write('<b>Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна):</b>'+ '<br><br>');   
